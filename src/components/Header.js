ec7e11cc712e7c657804e986f08db42011d203dd
smagucha <smagucha@gmail.com>
2022-01-13 02:27:31 +0300

    main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,43 @@
 import './css/nav.css';
+import React, {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
-function Header() {
+
+export default function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
+
   return (
-    <div >
-     <ul>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/">Home</Link></li>
+    <nav>
+      {(toggleMenu || screenWidth > 500) && (
+       <ul>
+       <li><Link to="/">Home</Link></li>
+       <li><Link to="/about">About</Link></li>
+       <li><Link to="/projects">Projects</Link></li>
+      <li><Link to="/contact">Contact</Link></li> 
     </ul>
-    </div>
-  );
+      )}
+
+      <button onClick={toggleNav} className="btn">Menu</button>
+    </nav>
+  )
 }
-
-export default Header;
-
-
-
-
 
