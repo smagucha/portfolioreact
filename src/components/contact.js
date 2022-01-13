2821d1a4e323cfd448c14a0bb7ada0abebac34b8
smagucha <smagucha@gmail.com>
2022-01-13 06:04:42 +0300

    form changes

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,21 +1,13 @@
 import Nav from './Header';
 import Footer from './Footer';
 import './css/form.css';
-import emailjs from 'emailjs-com'
-
-const Contact =() =>{
-	function sendEmail(e){
-	e.preventDefault();
-	emailjs.sendForm('service_qn1u783','template_aehly3p',e.target,'user_UuAa4NbrvYOzVpzJBGtvU').then(res =>{
-		console.log(res);
-	}).catch(err=> console.log(err));
-}
+function Contact(){
 	return (
 		<div >
             <Nav/>
             <div className ='maincontent' > 
             <div className='formdiv'>
-            	<form onSubmit={sendEmail}>
+            	<form >
             		<p><label>your fullname</label>
             		<input type="text" name="name"/></p>
             		<p><label>email</label>
